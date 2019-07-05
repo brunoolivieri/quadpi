18682d54e82524f66ffecf2e35571ed271634c8e
Bruno Olivieri <bruno@olivieri.com.br>
2019-07-05 15:46:56 -0300

    Adjusting installer.

--- install_sitl.sh
+++ install_sitl.sh
@@ -8,13 +8,11 @@
 
 
 # Updating until this date -----------------------------------------------
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get update -y
+sudo apt-get upgrade -y
 
 # Creates a folder to organize stuff -------------------------------------
-cd ~
-mkdir sitl
-cd sitl
+cd ..
 
 # Downloads full Ardupilot repository ------------------------------------
 git clone --recurse-submodules https://github.com/ArduPilot/ardupilot.git
@@ -24,23 +22,32 @@ git submodule update --init --recursive
 # Reuses Build environment to install Ubuntu and Pythonpackages ----------
 ./Tools/environment_install/install-prereqs-ubuntu.sh 
 
+# Install mavproxy
+pip install --upgrade pymavlink MAVProxy --user
+
 # Fixing small stuff -----------------------------------------------------
 echo "export PATH=$PATH:$HOME/.local/bin" >> ~/.bashrc
 source ~/.bashrc
 
 # Adding sites to default locations
-echo "AbraDF=-15.839650568136552,-47.92712785889222,1042,30" >> /Tools/autotest/locations.txt
+echo "AbraDF=-15.839650568136552,-47.92712785889222,1042,30" >> ./Tools/autotest/locations.txt
 
 # Running ----------------------------------------------------------------
-cd ~/sitl/ardupilot/ArduCopter/
+cd ArduCopter/
 ../Tools/autotest/sim_vehicle.py -L AbraDF --console --map
 
 # Hint/Tip to reuse ------------------------------------------------------
 echo " "
 echo " "
 echo " To run the simulator again: "
-echo " cd ~/sitl/ardupilot/ArduCopter/"
+echo " cd ardupilot/ArduCopter/"
 echo " ../Tools/autotest/sim_vehicle.py --console --map"
+
+
+
+
+
+
 
 
 
--- run_sitl.sh
+++ run_sitl.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cd ~/sitl/ardupilot/ArduCopter/
+cd ../ardupilot/ArduCopter/
 ../Tools/autotest/sim_vehicle.py -L AbraDF --console --map
